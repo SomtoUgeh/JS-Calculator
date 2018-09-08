9793347c5d59cf5d514c536578d7b31b2527138e
somtougeh <smugeh@gmail.com>
2018-09-08 16:31:47 +0100

    Change to calculator display

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 const calculator = document.querySelector(".calculator");
 const calculator__keys = document.querySelector(".calculator__keys");
+const calculator__display = document.querySelector(".calculator__display");
 
 calculator__keys.addEventListener("click", e => {
-  if (e.target.matches("button")) {
-    const key = e.target;
-    const action = key.dataset.action;
+	if (e.target.matches("button")) {
+		const key = e.target;
+		const action = key.dataset.action;
+		const keyContent = key.textContent;
+		const displayedNum = calculator__display.textContent;
 
-if (!action) {
-  console.log("number key");
-}
+		if (!action) {
+			if (displayedNum == "0") {
+				calculator__display.textContent = keyContent;
+			} else {
+				calculator__display.textContent = displayedNum + keyContent;
+			}
+		}
 
-if (
-  action === "add" ||
-  action === "subtract" ||
-  action === "multiply" ||
-  action === "divide"
-) {
-  console.log("Operation key");
-}
+		if (
+			action === "add" ||
+			action === "subtract" ||
+			action === "multiply" ||
+			action === "divide"
+		) {
+			console.log("Operation key");
+		}
 
-if (action === "decimal") {
-  console.log("decimal");
-}
+		if (action === "decimal") {
+			console.log("decimal");
+		}
 
-if (action === "clear") {
-  console.log("clear");
-}
+		if (action === "clear") {
+			console.log("clear");
+		}
 
-if (action === "calculate") {
-  console.log("calculate");
-}
-  }
+		if (action === "calculate") {
+			console.log("calculate");
+		}
+	}
 });
