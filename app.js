f1bd0d19fcc7dfe705a47e6537d233874397cb94
somtougeh <smugeh@gmail.com>
2018-09-15 11:49:33 +0100

    Refactor complete

--- app.js
+++ app.js
@@ -3,71 +3,18 @@ const calculator__keys = document.querySelector(".calculator__keys");
 const calculator__display = document.querySelector(".calculator__display");
 
 calculator__keys.addEventListener("click", e => {
-	// Check for all instances of <button>
-	if (e.target.matches("button")) {
-		const key = e.target;
-		const action = key.dataset.action;
-		const keyContent = key.textContent;
-		const displayedNum = calculator__display.textContent;
-		const previousKeyType = calculator.dataset.previousKeyType;
-
-		// Make an array and remove 'is-depressed' state from each key
-		Array.from(key.parentNode.children).forEach(k =>
-			k.classList.remove("is-depressed")
-		);
-
-		// Splitting code into pure and impure functions
-
-		// Pure function
-		const resultString = createResultString();
-
-		// Impure function
-		updateCalculatorState();
-
-		if (
-			action === "add" ||
-			action === "subtract" ||
-			action === "multiply" ||
-			action === "divide"
-		) {
-			// Make custom datasets for holding information
-			calculator.dataset.operation = action;
-			calculator.dataset.previousKeyType = "operator";
-			key.classList.add("is-depressed");
-		}
-
-		if (action === "clear") {
-			if (key.textContent === "AC") {
-				calculator.dataset.firstValue = "";
-				calculator.dataset.modValue = "";
-				calculator.dataset.operator = "";
-				calculator.dataset.previousKeyType = "";
-			} else {
-				key.textContent = "AC";
-			}
-			calculator.dataset.previousKeyType = "clear";
-		}
-
-		if (action !== "clear") {
-			const clearButton = calculator.querySelector("[data-action=clear]");
-			clearButton.textContent = "CE";
-		}
-
-		if (action === "calculate") {
-			calculator.dataset.modValue = secondValue;
-			calculator.dataset.previousKeyType = "calculate";
-		}
-	}
-});
-
-calculator__keys.addEventListener("click", e => {
 	if (e.target.matches("button")) return;
+	const key = e.target;
 	const displayedNum = calculator__display.textContent;
 	const resultString = createResultString(
-		e.target,
+		key,
 		displayedNum,
 		calculator.dataset
 	);
+
+	calculator__display.textContent = resultString;
+	updateCalculatorState(key, calculator, resultString, displayedNum);
+	updateVisualState(key, calculator);
 });
 
 createResultString = (key, displayedNum, state) => {
@@ -114,7 +61,57 @@ createResultString = (key, displayedNum, state) => {
 	}
 };
 
-const getKeyType = key => {
+updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+	const keyType = getKeyType(key);
+	calculate.dataset.previousKeyType = keyType;
+
+	if (keyType === "operator") {
+		calculator.dataset.operator = key.dataset.action;
+		calculator.dataset.firstValue =
+			firstValue &&
+			operator &&
+			previousKeyType !== "operator" &&
+			previousKeyType !== "calculate"
+				? calculatedValue
+				: displayedNum;
+	}
+
+	if (action === "clear") {
+		if (key.textContent === "AC") {
+			calculator.dataset.firstValue = "";
+			calculator.dataset.modValue = "";
+			calculator.dataset.operator = "";
+			calculator.dataset.previousKeyType = "";
+		}
+	}
+
+	if (keyType === "calculate") {
+		calculator.dataset.modValue =
+			firstValue && previousKeyType === "calculate"
+				? modValue
+				: displayedNum;
+	}
+};
+
+updateVisualState = (key, calculator) => {
+	const keyType = getKeyType(key);
+	Array.from(key.parentNode.children).forEach(k =>
+		k.classList.remove("is-depressed")
+	);
+
+	if (keyType === "operator") key.classList.add("is-depressed");
+
+	if (keyType === "clear" && key.textContent !== "AC") {
+		key.textContent = "AC";
+	}
+
+	if (keyType !== "clear") {
+		const clearButton = calculator.querySelector("[data-action=clear]");
+		clearButton.textContent = "CE";
+	}
+};
+
+getKeyType = key => {
 	const { action } = key.dataset;
 	if (!action) return "number";
 	if (
@@ -128,7 +125,7 @@ const getKeyType = key => {
 	return action;
 };
 
-const calculate = (n1, operator, n2) => {
+calculate = (n1, operator, n2) => {
 	const firstNum = parseFloat(n1);
 	const secondNum = parseFloat(n2);
 	if (operator === "add") return firstNum + secondNum;
